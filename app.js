e52228d08df3ec4fc82ca8125969bc0d622b2f4a
Mamoungg <gasmi.mamoun@gmail.com>
2021-11-20 11:39:01 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Intial Count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = "blue";
+        }
+        if(count < 0) {
+            value.style.color = 'red';
+        }
+        if(count > 9) {
+            value.style.color = "green";
+        }
+        if(count > 10) {
+            value.style.color = "blue "
+        }
+        value.textContent = count;
+    })
+});
